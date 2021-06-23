da22bd04c3da0ba85c81bdb7f35c20aa3dc349e1
Andy M <genesis@WOPR.local>
2021-06-23 13:10:40 +0200

    updated git ignore and forgot to save main.m

--- main.m
+++ main.m
@@ -1,31 +1,13 @@
 %%FILE I/O SECTION
 
-%TODO: This Folder location and subfolder will need to be an input coming from
-%the GUI!
-
-base_path='./Datasets';
-sub_folders=dir(base_path);
-
-
-
-location='Frauenkirche'; %This will also have to be User selectable
-
-target_folder=fullfile(base_path,location);
-
-if ~isdir(target_folder)
-  errorMessage = sprintf('Error: The target folder does not exist:\n%s', target_folder);
-  uiwait(warndlg(errorMessage));
-  return;
-end
-
-
+%Start UI from main.m file
+app=UI();
 
 %find all image files in the target_folder and count how many there are
 imageFiles=dir([target_folder '/*.jpg']);
 n_images=length(imageFiles);
 
-features={};
-valid_points={};
+
 %open each image in target_folder, and process it
 for k = 1 : n_images
     %% IMAGE LOADING AND PREPROCESSING
@@ -41,6 +23,7 @@ for k = 1 : n_images
     %Match curerent image histograms to the first image (ref_image)
     current_image_corr=imhistmatch(current_image,ref_image);
     
+    %grayscale image if it isn't already
     if size(current_image_corr,3)~=1
         gray_image=rgb2gray(current_image_corr);
     else
@@ -51,12 +34,23 @@ for k = 1 : n_images
     
     imageArray(:,:,k)=gray_image;
     
-    %% Extract Features from image
-    points=detectORBFeatures(gray_image);
-    
-    [features,validPoints(:,;,k)] = extractFeatures(gray_image,points,'Method', 'ORB');
-    
 end
 
 
 
+%% Extract Features from image
+
+if visualization_type=='first_last'
+    points1= detectBRISKFeatures(imageArray(:,:,1));
+    points2= detectBRISKFeatures(imageArray(:,:,end));
+    
+    [features1,valid_points1] = extractFeatures(imageArray(:,:,1),points1,'Method', 'BRISK');
+    [features2,valid_points2] = extractFeatures(imageArray(:,:,end),points2,'Method', 'BRISK');
+ 
+    index=matchFeatures(features1,features2,'MatchThreshold',25.0,'MaxRatio',0.6);
+    
+    matchedPoints1 = valid_points1(index(:,1));
+    matchedPoints2 = valid_points2(index(:,2));
+    figure;
+    showMatchedFeatures(imageArray(:,:,1),imageArray(:,:,end),matchedPoints1,matchedPoints2);
+end
