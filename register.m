58b4b8687c0554454ecc454205fa16f0051c5bad
Andy M <genesis@WOPR.local>
2021-06-23 17:01:57 +0200

    moved registration to its own function, added visualization type radiobuttons to the UI that dont do very much at the moment.

--- register.m
+++ register.m
@@ -1,41 +1,4 @@
-%%FILE I/O SECTION
-
-%Start UI from main.m file
-app=UI();
-
-% %find all image files in the target_folder and count how many there are
-% imageFiles=dir([target_folder '/*.jpg']);
-% n_images=length(imageFiles);
-%
-
-%open each image in target_folder, and process it
-for k = 1 : n_images
-    %% IMAGE LOADING AND PREPROCESSING
-    baseFileName = imageFiles(k).name;
-    fullFileName = fullfile(imageFiles(k).folder, baseFileName);
-
-    current_image= imread(fullFileName);
-
-    if k==1
-        ref_image=current_image;
-    end
-
-    %Match curerent image histograms to the first image (ref_image)
-    current_image_corr=imhistmatch(current_image,ref_image);
-
-    %grayscale image if it isn't already
-    if size(current_image_corr,3)~=1
-        gray_image=rgb2gray(current_image_corr);
-    else
-        gray_image=current_image_corr;
-    end
-
-
-
-    imageArray(:,:,k)=gray_image;
-
-end
-
+function [registered_image] = register(reference_image,moved_image)
 %% Register images and align
 
 %detect features via surf and extract feature descriptors via BRISK
@@ -66,4 +29,5 @@ rot_registered = imwarp(rotated,tform,'OutputView',imref2d(size(original)));
 %show how the reconstructed image and the first one line up.
 figure
 imshowpair(original, rot_registered,'diff');
+end
 
