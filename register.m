005536c7f1f7b4df82ce1d5f251679421c4cb39c
Andy M <genesis@WOPR.local>
2021-06-25 17:36:18 +0200

    register now also puts out transforms so they can be applied to color images. timelapse makes registered image array and a timelapse movie object, will see what can be displayed best in the UI

--- register.m
+++ register.m
@@ -1,16 +1,31 @@
-function [registered_image] = register(reference_image,moved_image)
+function [tform, registered_image] = register(reference_image,moved_image, relax_flag)
 %% Register images and align
 
+%relax matching criteria if hard to match
+if relax_flag==0
+    match_threshold=10;
+    max_ratio=0.6;
+elseif relax_flag==1
+    match_threshold=25;
+    max_ratio=0.75;
+end
+
+
 %detect features via surf and extract feature descriptors via BRISK
-points1= detectSURFFeatures(reference_image);
-points2= detectSURFFeatures(moved_image);
+points1= detectKAZEFeatures(reference_image);
+points2= detectKAZEFeatures(moved_image);
 
 [features1,valid_points1] = extractFeatures(reference_image,points1,'Method', 'BRISK');
 [features2,valid_points2] = extractFeatures(moved_image,points2,'Method', 'BRISK');
 
 
 %match corresponding features
-index=matchFeatures(features1,features2,'MatchThreshold',25.0,'MaxRatio',0.6);
+if size(valid_points1,1)>5000 || size(valid_points2,1)>5000
+    index=matchFeatures(features1,features2,'MatchThreshold',25.0,'MaxRatio',0.6, 'Method', 'Approximate');
+else
+    index=matchFeatures(features1,features2,'MatchThreshold',25.0,'MaxRatio',0.6);
+end
+
 
 matchedPoints1 = valid_points1(index(:,1));
 matchedPoints2 = valid_points2(index(:,2));
@@ -21,13 +36,13 @@ matchedPoints2 = valid_points2(index(:,2));
 
 %fit a transform between the points, then find the invers of it
 tform = fitgeotrans(matchedPoints2.Location,matchedPoints1.Location,'NonreflectiveSimilarity');
-inv_tform=invert(tform);
+
 
 %apply the inverse to the rotated image 
 registered_image = imwarp(moved_image,tform,'OutputView',imref2d(size(reference_image)));
 
 %show how the reconstructed image and the first one line up.
-% figure
-% imshowpair(original, rot_registered,'diff');
+%  figure
+%  imshowpair(reference_image, registered_image,'diff');
 end
 
--- timelapse.m
+++ timelapse.m
@@ -1,7 +1,40 @@
-function [image_Array] = timelapse(target_folder)
+function [image_Array, time_lapse] = timelapse(target_folder)
 %TIMELAPSE Summary of this function goes here
 %   Detailed explanation goes here
 
-image_Array=imread('pears.png');
+imageFiles=dir([target_folder '/*.jpg']);
+n_images=length(imageFiles);
+image_Array{n_images}=[];
+
+image_Array{1}=imread(fullfile(target_folder,imageFiles(1).name));
+time_lapse(1)=im2frame(image_Array{1});
+%open each image in target_folder, and process it
+for k = 2 : n_images
+    
+    ref_image=rgb2gray(image_Array{1});
+    
+    fullFileName = fullfile(target_folder, imageFiles(k).name);
+    color_image= imread(fullFileName);
+    
+
+    %TODO: add preprocess step!
+    %Match curerent image histograms to the first image (ref_image)
+    current_image_corr=imhistmatch(color_image,ref_image);
+    
+    %grayscale image if it isn't already
+    if size(current_image_corr,3)~=1
+        gray_image=rgb2gray(current_image_corr);
+
+    else
+        gray_image=current_image_corr;
+    end
+    
+    
+    [tform, ~]=register(ref_image,gray_image,0);
+    
+    image_Array{k}=imwarp(color_image,tform,'OutputView',imref2d(size(ref_image,[1 2])));
+    
+    time_lapse(k)=im2frame(image_Array{k});
+end
 end
 
